9f521841a0395fab4f7d4d5198b9b6a76ee9d058
massarellijr <mauriciomassarellijr@gmail.com>
2026-07-07 10:24:56 -0300

    ajustando servidor para servir paginas html

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Servidor esta rodando!');
-});
+// Configura o servidor para entregar os arquivos da pasta public
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, '0.0.0.0', () => {
   console.log('Servidor rodando na porta ' + port);
