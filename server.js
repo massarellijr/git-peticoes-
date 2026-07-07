aaecf92bf16625770c0e630a26dabb01f292491f
massarellijr <mauriciomassarellijr@gmail.com>
2026-07-07 10:01:12 -0300

    corrigindo erro de inicializacao do servidor

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-
-// Serve arquivos da pasta 'public'
-app.use(express.static('public'));
-// Substitua sua linha atual de app.listen por esta:
 const port = process.env.PORT || 3000;
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Servidor rodando na porta ${port}`);
+app.get('/', (req, res) => {
+  res.send('Servidor esta rodando!');
 });
 
-
+app.listen(port, '0.0.0.0', () => {
+  console.log('Servidor rodando na porta ' + port);
 });
